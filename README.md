62e5321f19eb432f430b283811236bb4a2d81c86
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-03-10 11:51:29 -0800

    first commit

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## const url = 'https://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid='
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import M from 'materialize-css';
+import axios from 'axios'
+import './App.css'
+import weather from './weather.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './Header';
+import Body from './Body';
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      temp: '',
+      cityName: '',
+      weather: '',
+      high: '',
+      low: '',
+      icon: '',
+      showModal: false,
+    }
+  }
+  // GET API
+  // Do not put in render!! 
+  componentDidMount=()=>{
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=Vancouver&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`
+    // console.log(url)
+    // axios.get(url).then((response) => {
+    //   this.setState({
+    //     temp: response.data.main.temp,
+    //     cityName: response.data.name,
+    //     weather: response.data.decription,
+    //     high: response.data.main.temp_max,
+    //     low: response.data.main.temp_min,
+    //     icon: response.data.weather[0].icon,
+    //   })
+    // })
+    this.setState({
+      temp: weather[0].main.temp,
+      cityName: weather[0].name,
+      weather: weather[0].weather[0].description,
+      high: weather[0].main.temp_max,
+      low: weather[0].main.temp_min,
+      icon: weather[0].weather[0].icon,
+    })
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  }
+
+  render () {
+    const iconUrl = `http://openweathermap.org/img/w/${this.state.icon}.png`
+
+    return (
+      <div className="App">
+        <Header cityName={this.state.cityName} temp={this.state.temp} />
+        <Body iconUrl={iconUrl} cityName={this.state.cityName} high={this.state.high} low={this.state.low} weather={this.state.weather} />
+      </div>
+    )
+  }
 }
 
 export default App;
